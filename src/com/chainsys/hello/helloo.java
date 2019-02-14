da427bea862c723f398912a2d7e9c708652bd87f
durg2128 <durg2128@CSLH1708.global.chainsys.com>
2019-02-14 17:05:45 +0530

    new program

--- src/com/chainsys/hello/helloo.java
+++ src/com/chainsys/hello/helloo.java
@@ -1,5 +1,9 @@
-public class hello
+package com.chainsys.hello;
+public class helloo
 {
 public static void main(String args)
-System.out.println("Hello everyone");
+{
+	System.out.println("Hello everyone");
+}
+
 }
